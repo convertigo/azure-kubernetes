6457dbd9852b422b44cf7e5d968f42ba98c228a3
opic <olivierp@convertigo.com>
2018-06-07 18:37:28 +0200

    Convertigo on Azure AKS

---
+++ setkubednsname.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Public IP address
+IP="40.118.42.250"
+
+# Name to associate with public IP address
+DNSNAME="demo-c8o"
+
+# Get the resource-id of the public ip
+PUBLICIPID=$(az network public-ip list --query "[?ipAddress!=null]|[?contains(ipAddress, '$IP')].[id]" --output tsv)
+
+# Update public ip address with dns name
+az network public-ip update --ids $PUBLICIPID --dns-name $DNSNAME
